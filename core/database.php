58ad1fde16b64b0b5193331fbcff6f36e17adead
Abdullah Al Rifat <abdullahalrifat95@gmail.com>
2019-06-17 20:30:13 +0600

    notice board and adding notice added

--- core/database.php
+++ core/database.php
@@ -1,8 +1,8 @@
 <?php
     $servername = "localhost";
 $username = "root";
-$password = "";
-$dbname = "leavemanagement";
+$password = "151151";
+$dbname = "LeaveManagement";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);

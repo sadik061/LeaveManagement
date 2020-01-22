911ac273f2273de4e01492c041e2c06f6633f018
saif43 <saif.ahmed.anik.0@gmail.com>
2020-01-23 03:17:13 +0600

    Annual report added

--- app/report.php
+++ app/report.php
@@ -1,6 +1,13 @@
-<?php if (session_status() == PHP_SESSION_NONE) {
+<?php 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if (session_status() == PHP_SESSION_NONE) {
     session_start();
-} ?>
+} 
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
